5b59a32893002cbffa179ba781d5a0cb8790ba46
Fabiolegra <fabioargel3@gmail.com>
2025-11-16 22:19:46 -0300

    fix: define fuso-horario

--- bootstrap.php
+++ bootstrap.php
@@ -10,6 +10,8 @@
 // Carrega o autoloader do Composer
 require_once __DIR__ . '/vendor/autoload.php';
 
+//date_default_timezone_set('America/Sao_Paulo');
+
 // Carrega as variáveis de ambiente do arquivo .env para $_ENV
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
